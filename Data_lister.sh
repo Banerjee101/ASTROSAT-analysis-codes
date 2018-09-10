6cae9afa98376784baed057567de6ab90f8b3e86
Ankush Banerjee <43135608+Banerjee101@users.noreply.github.com>
2018-09-10 17:19:22 +0530

    Delete Data_lister.sh

--- Data_lister.sh
+++
@@ -1,8 +0,0 @@
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*A -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_A
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*Z -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_Z
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*S -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_S
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*R -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_R
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*G -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_G
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*C -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_C
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/*-*-*-*D -name "FS4f*" |  sort > RXTE_PCA_event_mode_list_D
-find /home/ankush/Desktop/Workspace_project/1A_2466-588/RXTE/Data/*/?????????????? -name "FS4f*" |  sort > RXTE_PCA_event_mode_list
